357442dce552790743c200dd07e84e54fdd2755e
aimedidierm <aimedidiermugisha@gmail.com>
2024-01-27 23:32:01 +0200

    Water meter empty card data

--- meter/meter.ino
+++ meter/meter.ino
@@ -10,7 +10,7 @@ LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 int Interrupt = 0;
 int sensorPin = 2;
 #define Valve A3
-#define red 
+#define red
 #define green 5
 #define buzzer 3
 float calibrationFactor = 90;
@@ -28,8 +28,8 @@ void (*resetFunc)(void) = 0;
 
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
-uint32_t cardValue = 0;
-int drinkvolume = 0; // Declare drinkvolume variable here
+String value = "";
+int drinkvolume = 0;
 
 void setup()
 {
@@ -86,53 +86,42 @@ void loop()
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
-  
-  uint32_t cardValue = 0;
-  for (uint8_t i = 0; i < 4; i++) {
-    cardValue |= (uint32_t)buffer1[i] << (8 * (3 - i));
+  // Delete all data on the block by writing zeros to it
+  byte zeros[16] = {0};
+  status = mfrc522.MIFARE_Write(block, zeros, 16);
+  if (status != MFRC522::STATUS_OK)
+  {
+    lcd.clear();
+    lcd.print("Write failed: ");
+    delay(1000);
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
   }
 
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    value += (char)buffer1[i];
+  }
+  value.trim();
   lcd.clear();
 
   // Convert the value to an integer and assign it to drinkvolume
-  drinkvolume = cardValue / 5; // Assuming conversion factor 5, adjust as needed
+  drinkvolume = value.toInt() / 5;
   lcd.setCursor(0, 0);
-  lcd.print(drinkvolume);
+  lcd.print(value.toInt());
   lcd.print("Rwf");
   lcd.setCursor(0, 1);
   lcd.print("Money loaded");
   delay(3000);
-
-  // Write 0 data onto the card
-  int newAmount = 0;
-
-  // Convert the new amount back to byte array
-  byte newBlockData[16];
-  for (int i = 0; i < 16; i++) {
-    if (i % 2 == 0) {
-      newBlockData[i] = (newAmount >> 8) & 0xFF;
-    } else {
-      newBlockData[i] = newAmount & 0xFF;
-    }
-  }
-
-  // Write data to the block
-  status = mfrc522.MIFARE_Write(blockNum, newBlockData, 16);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Writing to Block failed: ");
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  } else {
-    Serial.println("Data was written into Block successfully");
+  if(drinkvolume == 0){
     lcd.clear();
-    lcd.print("Money added");
-    digitalWrite(green, HIGH);
-    delay(4000);
+    lcd.setCursor(0, 1);
+    lcd.print("Card Low balance");
+    delay(3000);
     resetFunc();
   }
-  
   mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1(); 
+  mfrc522.PCD_StopCrypto1();
   waterout();
 }
 
@@ -160,10 +149,10 @@ void waterout()
   }
   digitalWrite(Valve, HIGH);
   drinkvolume = 0;
+  resetFunc();
 }
 
 void pulseCounter()
 {
   pulseCount++;
 }
-
